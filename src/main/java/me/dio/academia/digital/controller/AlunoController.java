22648ddc6aebfec749e75dd3acdff22c52a2c841
Luis Felipe <1luisfelipe2@gmail.com>
2022-05-05 11:01:55 -0400

    add search queries for Aluno

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -3,8 +3,11 @@ package me.dio.academia.digital.controller;
 import me.dio.academia.digital.entity.Aluno;
 import me.dio.academia.digital.entity.AvaliacaoFisica;
 import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.repository.AlunoRepository;
 import me.dio.academia.digital.service.impl.AlunoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -13,6 +16,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
+  @Autowired
+  private AlunoRepository alunoRepository;
 
   @Autowired
   private AlunoServiceImpl service;
@@ -30,6 +36,12 @@ public class AlunoController {
   @GetMapping
   public List<Aluno> getAll(@RequestParam(value = "dataDeNascimento", required = false) String dataDeNacimento) {
     return service.getAll(dataDeNacimento);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Aluno>> getAll(@Valid Aluno aluno) {
+    List<Aluno> all = alunoRepository.findAll(Example.of(aluno));
+    return ResponseEntity.ok(all); 
   }
 
 }
